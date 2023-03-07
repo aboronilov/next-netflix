f71ce7365ea504ade74654ce75f9fbaec61dbaa1
Anatoly <boronilov@gmail.com>
2023-03-07 11:37:08 +0200

    Project init. Auth UI

---
+++ pages/auth.tsx
@@ -0,0 +1,79 @@
+import Input from "@/components/Input";
+import Image from "next/image";
+import React, { useCallback, useState } from "react";
+
+type Props = {};
+
+const Auth = (props: Props) => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <Image
+            src="/images/logo.png"
+            alt="Netflix logo"
+            width={120}
+            height={120}
+          />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign In" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  id="username"
+                  value={username}
+                />
+              )}
+              <Input
+                label="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Sign Up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account?" : "Login"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
