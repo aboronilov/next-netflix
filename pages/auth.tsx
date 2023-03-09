fa8d8dbaffa1c5daeeca07901f59ea8db216de70
Anatoly <boronilov@gmail.com>
2023-03-09 16:49:03 +0200

    google and github auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,13 +1,31 @@
-import Input from "@/components/Input";
-import Image from "next/image";
-import React, { useCallback, useState } from "react";
 import axios from "axios";
-import { signIn } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
-type Props = {};
+import Input from "@/components/Input";
 
-const Auth = (props: Props) => {
+// export async function getServerSideProps(context: NextPageContext) {
+//   const session = await getSession(context);
+
+//   if (session) {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       }
+//     }
+//   }
+
+//   return {
+//     props: {}
+//   }
+// }
+
+const Auth = () => {
   const router = useRouter();
 
   const [email, setEmail] = useState("");
@@ -32,22 +50,22 @@ const Auth = (props: Props) => {
       });
 
       router.push("/");
-    } catch (error: any) {
-      console.log(`Login error: ${error.message}`);
+    } catch (error) {
+      console.log(error);
     }
   }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
-      await axios.post("/api/register/", {
+      await axios.post("/api/register", {
         email,
         name,
         password,
       });
 
-      login()
-    } catch (error: any) {
-      console.log(`Register error: ${error.message}`);
+      login();
+    } catch (error) {
+      console.log(error);
     }
   }, [email, name, password, login]);
 
@@ -55,49 +73,58 @@ const Auth = (props: Props) => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <Image
-            src="/images/logo.png"
-            alt="Netflix logo"
-            width={120}
-            height={120}
-          />
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Sign In" : "Register"}
+              {variant === "login" ? "Sign in" : "Register"}
             </h2>
             <div className="flex flex-col gap-4">
               {variant === "register" && (
                 <Input
-                  label="Username"
-                  onChange={(e) => setName(e.target.value)}
-                  id="username"
-                  value={name}
+                  id="name"
                   type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
               <Input
-                label="Email"
-                onChange={(e) => setEmail(e.target.value)}
                 id="email"
-                value={email}
                 type="email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                label="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                id="password"
-                value={password}
                 type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
             <button
-              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
               onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
-              {variant === "login" ? "Login" : "Sign Up"}
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={32} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={32} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
@@ -106,9 +133,16 @@ const Auth = (props: Props) => {
                 onClick={toggleVariant}
                 className="text-white ml-1 hover:underline cursor-pointer"
               >
-                {variant === "login" ? "Create an account?" : "Login"}
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
+              .
             </p>
+            <button
+              onClick={() => signOut()}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              Logout
+            </button>
           </div>
         </div>
       </div>
