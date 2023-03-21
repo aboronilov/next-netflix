5bc666492fcf8610d913528851e6cde7d67d61e6
Anatoly <boronilov@gmail.com>
2023-03-21 09:08:46 +0200

    mobile menu fixed

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 type Props = {
@@ -5,17 +6,43 @@ type Props = {
 };
 
 const MobileMenu = ({ visible }: Props) => {
+  const router = useRouter();
   if (!visible) {
     return null;
   }
   return (
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex transition duration-500 ease-in">
       <div className="flex flex-col gap-4">
-        <div className="px-3 text-center text-white hover:underline">Home</div>
-        <div className="px-3 text-center text-white hover:underline">Series</div>
-        <div className="px-3 text-center text-white hover:underline">Films</div>
-        <div className="px-3 text-center text-white hover:underline">New & Popular</div>
-        <div className="px-3 text-center text-white hover:underline">My List</div>
+        <div
+          className="px-3 text-center text-white hover:underline"
+          onClick={() => router.push("/")}
+        >
+          Home
+        </div>
+        <div
+          className="px-3 text-center text-white hover:underline"
+          onClick={() => router.push("/series")}
+        >
+          Series
+        </div>
+        <div
+          className="px-3 text-center text-white hover:underline"
+          onClick={() => router.push("/films")}
+        >
+          Films
+        </div>
+        <div
+          className="px-3 text-center text-white hover:underline"
+          onClick={() => router.push("/trending")}
+        >
+          New & Popular
+        </div>
+        <div
+          className="px-3 text-center text-white hover:underline"
+          onClick={() => router.push("/favorites")}
+        >
+          My List
+        </div>
       </div>
     </div>
   );
